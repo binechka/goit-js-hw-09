d4bbb868bccf8556acbf0a9ca205c2c6ba076037
Albina Bondar <albinabondar2019@gmail.com>
2022-08-17 01:47:56 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
 
+const startBtn = document.querySelector("button[data-start]")
+const stopBtn = document.querySelector("button[data-stop]")
+
+
+
+let setColorId = null;
+startBtn.addEventListener("click", () => {
+    setColorId = setInterval(getRandomHexColor, 1000);
+    startBtn.disabled = true;
+})
+
+const getRandomHexColor = () =>
+    document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(setColorId);
+    startBtn.disabled = false;
+})
