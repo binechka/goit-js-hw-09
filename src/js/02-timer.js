856b207547a09fc08575ffa8bac23817032a5225
Albina Bondar <albinabondar2019@gmail.com>
2022-08-18 21:19:58 +0300

    add task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,16 @@
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
+
 import "flatpickr/dist/flatpickr.min.css";
 
 const input = document.querySelector('#datetime-picker')
 const startBtn = document.querySelector("button[data-start]")
 const timer = document.querySelector(".timer")
+const dataDays = document.querySelector("span[data-days]")
+const dataHours = document.querySelector("span[data-hours]")
+const dataMinutes = document.querySelector("span[data-minutes]")
+const dataSeconds = document.querySelector("span[data-seconds]")
+
+
  startBtn.disabled = true
 const options = {
   enableTime: true,
@@ -12,7 +18,6 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      console.log(selectedDates[0]);
       
       if ((selectedDates[0].getTime() - options.defaultDate.getTime())<0) {
           window.alert("Please choose a date in the future")    
@@ -20,53 +25,46 @@ const options = {
       else {
           startBtn.disabled = false
       }
-
       startBtn.addEventListener("click", () => {
-    setInterval(() => {
-        const currentTime = Date.now();
+    const timerId = setInterval(() => {
+        const currentTime = new Date; 
         const deltaTime = selectedDates[0] - currentTime;
-        const { days, hours, minutes, seconds } = convertMs(deltaTime)
-        console.log(`${days}:${hours}:${minutes}:${seconds}`);
-    }, 1000)
+        const clocks = convertMs(deltaTime);
+         dataDays.textContent =  addLeadingZero(clocks.days);
+         dataHours.textContent =  addLeadingZero(clocks.hours);
+         dataMinutes.textContent =  addLeadingZero(clocks.minutes);
+        dataSeconds.textContent = addLeadingZero(clocks.seconds);
+         if ( deltaTime < 1000) {
+              clearInterval(timerId)
+          }
+    }, 1000)       
 })
-    },
-  
-  
+    }, 
 };
 
+function addLeadingZero (value) {
+return String(value).padStart(2, '0');    
+}
 
 flatpickr(input, options) 
 
-function updateClockFace({ days, hours, minutes, seconds }) {
-    timer.textContent = `${days}:${hours}:${minutes}:${seconds}`
-}
-
-function pad(value) {
-    return String(value).padStart(2, '0');
-}
-
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
+ 
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = pad(Math.floor(ms / day));
-  // Remaining hours
-  const hours = pad(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
 
 
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,27 @@
+const submitBtn = document.querySelector('[type="submit"]');
+const amountEl = document.querySelector('[name="amount"]');
+const stepEl = document.querySelector('[name="step"]');
+const delay = document.querySelector('[name="delay"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (shouldResolve)  resolve({ position, delay });
+     else  reject({ position, delay });
+  }, delay);
+})
 }
+
+submitBtn.addEventListener("click", event => {
+  event.preventDefault();
+  for (let i = 0; i < amountEl.value; i++) {
+    createPromise(i+1,  Number(delay.value) + Number(stepEl.value)*i)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  } 
+})
